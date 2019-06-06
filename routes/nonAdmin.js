5f4353d499a0280085fff35c56841a9a88c0b207
Kristanelle Calaguan <kristanelle.calaguan@gmail.com>
2019-06-06 12:12:06 +0800

    updated stripe payment and added stripe refund

--- routes/nonAdmin.js
+++ routes/nonAdmin.js
@@ -40,10 +40,14 @@ router.get('/bookings', (req, res, next) => {
 /* Create */
 router.post('/bookings', (req, res, next) => {
 	let carId = req.body.carId;
-	let startDate = moment(req.body.schedDate.startDate, "MM-DD-YYYY", true);
-	let endDate = moment(req.body.schedDate.endDate, "MM-DD-YYYY", true);
-
-	let rentedDays = (moment.duration(endDate.diff(startDate)).asDays());
+	let startDate = moment(req.body.startDate);
+	let endDate = moment(req.body.endDate);
+	let rentedDays = req.body.rentedDays;
+	let excessHours = req.body.excessHours;
+	let stripeToken = req.body.stripeToken;
+	let stripeTokenType = req.body.stripeTokenType;
+	let stripeEmail = req.body.stripeEmail;
+	let user = req.user.name;
 
 	if(!carId || !startDate || !endDate) {
 		return res.status(500).json({
@@ -51,7 +55,7 @@ router.post('/bookings', (req, res, next) => {
 		});
 	}
 
-	if(startDate.isBefore(moment()) || startDate.isBefore(moment())) {
+	if(startDate.isBefore(moment()) || endDate.isBefore(moment())) {
 		return res.status(500).json({
 			"error": "Cannot book on past dates"
 		})
@@ -75,20 +79,23 @@ router.post('/bookings', (req, res, next) => {
 			CarModel.findById(carId)
 			.then(car => {
 				let pricePerDay = car.price;
-				let totalPrice = (pricePerDay * rentedDays) * 100;
+				let proratedPrice = (pricePerDay / 24) * excessHours;
+				let totalPrice = (((pricePerDay * rentedDays)) + proratedPrice) * 100;
 
 				stripe.customers.create({
-					name: req.user.name,
-					email: req.user.email,
-					description: `Karen customer`,
-					source:"tok_mastercard"
+					name: user,
+					email: stripeEmail,
+					description: "KAREN customer",
+					source: "tok_visa"
 				})
+
 				.then(customer => {
 					stripe.charges.create({
 						amount: totalPrice,
 						currency: "php",
-						source: "tok_mastercard",
-						description: `Charge for ${car.brandMod}, rented for ${rentedDays} day(s)`,
+						source: customer.default_source,
+						// description: `Charge for ${car.brandMod}, rented for ${rentedDays} day(s)`,
+						customer: customer.id
 					})
 					.then(charges => {
 						BookingModel.create({
@@ -104,9 +111,9 @@ router.post('/bookings', (req, res, next) => {
 							chargeId: charges.id
 						})
 						.then(booking => {
-							CarModel.findById(carId)
+							CarModel.findById(booking.carId)
 							.then(car => {
-								car.bookings.push({carId: car._id});
+								car.bookings.push(booking._id);
 								car.save();
 
 								return res.status(200).json({
@@ -125,7 +132,7 @@ router.post('/bookings', (req, res, next) => {
 			.catch(next)
 		} else {
 			return res.status(500).json({
-				"message": `Found conflict with selected dates`
+				"error": "Found conflict with selected dates."
 			});
 		}
 	})
@@ -145,30 +152,37 @@ router.get('/bookings/:id', (req, res, next) => {
 router.delete('/bookings/:id', (req, res, next) => {
 	BookingModel.findById(req.params.id)
 	.then(booking => {
+		// Compare date and time now to booked startDate less 3 days
+		if(moment().isSameOrAfter(moment(booking.schedDate.startDate).subtract(3, 'd'))) {
+			// Throw error 422-Unprocessable Entity if  
+			return res.status(422).json({
+				"error": "Cannot cancel 3 days before scheduled start date."
+			});
+		}
 		stripe.charges.retrieve(booking.chargeId)
 		.then(charge => {
 			stripe.refunds.create({
 				charge: charge.id,
-				reason: "Cancelled booking"
+				reason: "requested_by_customer"
 			})
 			.then(refund => {
-				return res.json(refund)
 				BookingModel.create({
 					customerId: req.user._id,
-					carId: carId,
+					carId: booking.carId,
 					schedDate: {
-						startDate: startDate,
-						endDate: endDate
+						startDate: booking.startDate,
+						endDate: booking.endDate
 					},
 					transactionDate: moment(),
-					totalCharge: totalPrice,
-					transactionType: "cancellation",
-					chargeId: charges.id
+					totalCharge: booking.totalCharge,
+					transactionType: "Cancellation",
+					chargeId: refund.id
 				})
-				.then(booking => {
+				return res.json(refund)
+				.then(cancellation => {
 					CarModel.findById(booking.carId)
 					.then(car => {
-						car.bookings.pull({carId: car._id});
+						car.bookings.pull(booking._id);
 						car.save();
 
 						return res.status(200).json({
